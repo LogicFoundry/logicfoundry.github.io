27ed35e7ea5287816c28247f6107c27121b122a4
damianperera <15967502+damianperera@users.noreply.github.com>
2020-06-08 16:00:11 +0000

    Deploying to gh-pages from  @ 110e78d90847d01a9632ff49dbeaf4487a455d52 🚀

---
+++ precache-manifest.96489c4b997ad76bb6a885ac44dc8148.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c9e25b236fcea1906bc2cce442a5ad3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b781e44b712261eefdfb",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "602cc15237f6420fe23b",
+    "url": "/static/js/2.ee83eee4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ee83eee4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b781e44b712261eefdfb",
+    "url": "/static/js/main.2c7df95d.chunk.js"
+  },
+  {
+    "revision": "d1fd0b033f24c0719145",
+    "url": "/static/js/runtime-main.4d5d7fd5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
